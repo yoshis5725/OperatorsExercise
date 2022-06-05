eb6479a82738fb0ac5fe2280174afa05cb249de8
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-05 07:58:41 -0600

    initial commit

---
+++ OperatorsExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+        }
+    }
+}
+
